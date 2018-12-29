b560e45bf21059a0c49fe91ed89b5ad4dc153c58
Akshay Vala <akshayvala217@gmail.com>
2018-12-29 06:53:30 +0000

    main0.py edited online with Bitbucket

--- main0.py
+++ main0.py
@@ -5,8 +5,8 @@ try:
     file=open("Data.csv","r")
 except:
     with open("Data.csv","w") as file:
-        file.write("Id,Date,Type,Product,Quantity,Price_Per_Unit,Total_Price,Description\n")
-        file.close()
+     file.write("Date,Type,Product,Quantity,Price_Per_Unit,Total_Price,Description\n")
+     file.close()
 
 
 class Entry:
@@ -16,8 +16,7 @@ class Entry:
         self.price_per_unit=0.0
         self.description=None
         self.type_of_entry=None
-        self.date=None
-        self.total_price=0.0               #default initialization
+        self.date=None               #default initialization
 
     def set_product_name(self,product_name):
         self.product_name = product_name
@@ -34,24 +33,18 @@ class Entry:
     def set_date(self,date):
         self.date=date
 
-    def set_total_price(self):
-        self.total_price = (self.price_per_unit * self.quantity)
-
     """
     entry():
+
     method to write the entry in the file and will be called at last after setting all the attributes
+
     if anything goes wrong in setting the attributes and is not handled by code
-    then just don't call the method, entry will not be added to the file
+    then just don't call the method entry will not be added to the file
     """
     def entry(self):
-        file=open("Data.csv","r+")
-        list=file.readlines()
-        if (len(list)) == 1:
-         lastid=0
-        else:
-         lastid=int(list[-1].split(",")[0])
-        file.write(str(lastid+1) +"," +str(self.date)+","+ str(self.type_of_entry) + "," + str(self.product_name) + "," + str(self.quantity) + "," + str(self.price_per_unit) + "," + str(self.total_price) + "," +str(self.description) + "\n")
-
+        file=open("Data.csv","a")
+        file.write(str(self.date)+","+ str(self.type_of_entry) + "," + str(self.product_name) + "," + str(self.quantity) + "," + str(self.price_per_unit) + ","+str(self.quantity*self.price_per_unit)+"," +str(self.description) + "\n")
+        file.close()
     """
     delete(index)
     method to delete the entry and is to be called with "index" parameter i.e. id of entry that you
@@ -59,12 +52,12 @@ class Entry:
     """
     @staticmethod
     def delete(index):
-        file=pd.read_csv("Data.csv",index_col=0)
+        file=pd.read_csv("Data.csv",index_col=None)
         try:
-         file.drop(file.index[index-1], inplace=True)
+         file.drop(file.index[index],inplace=True)
         except IndexError:
          print("Index out of bound...!!!")
-        file.to_csv("Data.csv")
+        file.to_csv("Data.csv",index=False)
 
 
 
@@ -82,6 +75,7 @@ class Sell(Entry):
 
 while True:
     print("\nsell:1\nbuy:2\ndelete:3\nexit:4\n")
+    print(pd.read_csv("Data.csv"))
     c=int(input(":"))
     e=None
     if c==1:
@@ -100,7 +94,6 @@ while True:
         e.set_date(input("Date:\n"))
         e.set_quantity(int(input("Quantity:\n")))
         e.set_price_per_unit(float(input("Price per Unit:\n")))
-        e.set_total_price()
         e.set_description(input("Description:\n"))
         e.entry()
     except ValueError:
