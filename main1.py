5dfd6b5139d09853581d972d61d98f840f297c06
Keval25 <36570413+Keval25@users.noreply.github.com>
2019-01-31 14:51:46 +0530

    Commented on demand 😅

--- main1.py
+++ main1.py
@@ -1,34 +1,40 @@
+#importing required moduls
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 import pandas as pd
 
 
 class ExpenseCanvas(FigureCanvas):
+    """A child which inherits lots of behaviours from parent to generate graph canvas 🙂"""
+
     def __init__(self, parent=None, width=5, height=5, dpi=100):
+        """"Well, that is where child uses inherited behaviours for his one and only aim of his life, embedding graph to canvas indeed !!😄""""
+
         fig = Figure(figsize=(width, height), dpi=dpi)
         self.axes = fig.add_subplot(111)
-
+        
+        #And here he is done with his task 😌
         FigureCanvas.__init__(self, fig)
-        self.setParent(parent)  # Generating canvas for plotting of the graph
-
+        self.setParent(parent)  
         self.plot_expense()
 
+        #Variables which are yet to assign with their values
         self.df = None
         self.gbe = None
-        self.buy = None  # Variable initialization
+        self.buy = None  
 
     def plot_expense(self):
-        """A function to plot a bar chart from appropriate data from Data.csv for expense"""
+        """"One and only aim of this method is to serve a beutiful graph made with all processed ingredients 🤩"""
 
         ax = self.figure.add_subplot(111)
-        # Preparing data
+
+        # Here, all the chopping of vegetables happen to cook a dish 
         self.df = pd.read_csv('Data.csv', index_col=None)
         self.gbe = self.df.groupby(['Type', 'Product'], as_index=False)[['Quantity', 'Total_Price']].sum()
         self.buy = self.gbe[self.gbe.Type == 'Buy']
         self.buy['Total_Price'] = -self.buy['Total_Price']
 
-        # Plotting of the bar
-        # self.buy.plot.bar(x='Product', y='Total_Price',title='Product wise expenses', color='#2980b9', fontsize=12)
+        #And here all the processed ingredients gone to the hands of cook(matplotlib).
         ax.bar(self.buy['Product'].values, self.buy['Total_Price'].values)
         ax.set_title('Expense')
         ax.set_xlabel('Product')
@@ -36,12 +42,16 @@ class ExpenseCanvas(FigureCanvas):
 
 
 class ProfitCanvas(FigureCanvas):
+    """Well come to the class were we show employer his own diligence, a profit graph indeed !!😌"""
+
     def __init__(self, parent=None, width=5, height=5, dpi=100):
+        """Have nothing to say..After explaining same for the ExpenseCanvas 🙂"""
+
         fig = Figure(figsize=(width, height), dpi=dpi)
         self.axes = fig.add_subplot(111)
 
         FigureCanvas.__init__(self, fig)
-        self.setParent(parent)  # Generating canvas for plotting of the graph
+        self.setParent(parent)  
 
         self.plot_profit()
 
@@ -49,15 +59,17 @@ class ProfitCanvas(FigureCanvas):
         self.gbs = None
 
     def plot_profit(self):
-        """A function to plot a bar chart from appropriate data from Data.csv for profit"""
+        """One and only task of this method is to make employer happy by showing him a visual representation of his diligence !!🤗"""
 
         ax = self.figure.add_subplot(111)
-        # Preparing data
+
+        # And here it collects and form data required to plot a graph
         self.df = pd.read_csv('Data.csv', index_col=None)
         self.gbs = self.df.groupby('Product', as_index=False)[['Product', 'Total_Price']].sum()
 
-        # Plotting of the bar
+        # Handed on to the master for creating graph
         ax.bar(self.gbs['Product'].values, self.gbs['Total_Price'].values)
         ax.set_title('Profit')
         ax.set_xlabel('Product')
         ax.set_ylabel('Profit in ₹')
+
