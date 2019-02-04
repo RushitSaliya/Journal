42fa20d4cce836a7a751ff18c672cf7624ced30b
Akshay Vala <40417281+akshayvala@users.noreply.github.com>
2019-02-04 23:41:17 +0530

    Some enhancements...

--- main0.py
+++ main0.py
@@ -1,8 +1,7 @@
-from datetime import datetime
-from dateutil.parser import parse
 import pandas as pd
 
 file = None
+"""Trying to open existing file if not found (in the case of first time execution) new file will be created."""
 try:
     file = open("Data.csv", "r")
 except FileNotFoundError:
@@ -10,19 +9,13 @@ except FileNotFoundError:
         file.write("Date,Type,Product,Quantity,Price_Per_Unit,Total_Price,Description\n")
         file.close()
 
-try:
-    file = open("dataToDoList.csv", "r")
-except FileNotFoundError:
-    with open("dataToDoList.csv", "w+") as file:
-        file.write("Time,Description\n")  # file for data entry of remainders
-        file.close()
-
 
 class Entry:
     total_buy = 0.0
-    total_sell = 0.0
+    total_sell = 0.0  # inititalization of class variables
 
     def __init__(self, date="", type_of_entry="", product_name="", quantity=0, price_per_unit=0.0, description=""):
+        """For initializing the parameters."""
         self.product_name = product_name
         self.quantity = quantity
         self.price_per_unit = price_per_unit
@@ -31,14 +24,8 @@ class Entry:
         self.type_of_entry = type_of_entry
         self.date = date
 
-    """
-    entry():
-    method to write the entry in the file and will be called at last after setting all the attributes
-    if anything goes wrong in setting the attributes and is not handled by code
-    then just don't call the method, entry will not be added to the file
-    """
-
     def entry(self):
+        """This method will write an entry in the file and should be called at last after setting all the attributes"""
         datafile = open("Data.csv", "a")
         if self.type_of_entry == 'Buy':
             self.total_price = -(self.quantity * self.price_per_unit)
@@ -49,25 +36,16 @@ class Entry:
             self.description) + "\n")
         datafile.close()
 
-    """
-    delete(index)
-    method to delete the entry and is to be called with "index" parameter i.e. id of entry that you
-    want to delete
-    """
-
     @staticmethod
     def delete(delete_index):
+        """This will delete the entry at specified index i.e. delete_index parameter"""
         datafile = pd.read_csv("Data.csv", index_col=None)
         datafile.drop(datafile.index[delete_index], inplace=True)
         datafile.to_csv("Data.csv", index=False)
 
-    """
-    total_buy_sell():
-    it will update two class variables buydata & selldata which are total of buy & sell respectively...
-    should be called with each entry
-    """
     @staticmethod
     def total_buy_method():
+        """This method will calculate total amount of purchase and must be called after each buy data entry..."""
         datafile = pd.read_csv("Data.csv")
         buydata = datafile.where(datafile["Type"] == "Buy").iloc[:, 5]
         Entry.total_buy = -buydata.sum()
@@ -75,90 +53,23 @@ class Entry:
 
     @staticmethod
     def total_sell_method():
+        """This method will calculate total amount of sell and must be called after each sell data entry..."""
         datafile = pd.read_csv("Data.csv")
         selldata = datafile.where(datafile["Type"] == "Sell").iloc[:, 5]
         Entry.total_sell = selldata.sum()
         return Entry.total_sell
 
 
-class ToDo:
-    tasks = {}
-
-    def __init__(self):
-        self.day = datetime.now().day
-        self.month = datetime.now().month
-        self.year = datetime.now().year
-        self.hour = datetime.now().hour
-        self.minute = datetime.now().minute
-        self.description = None  # default initialization(current time)
-
-    def set_day(self, day):
-        self.day = day
-
-    def set_month(self, month):
-        self.month = month
-
-    def set_year(self, year):
-        self.year = year
-
-    def set_hour(self, hour):
-        self.hour = hour
-
-    def set_minute(self, minute):
-        self.minute = minute
-
-    def set_description(self, description):
-        self.description = description  # setter methods for giving time of remainders
-
-    @staticmethod
-    def delete_task(delete_task_index):
-        tododatafile = pd.read_csv("dataToDoList.csv", index_col=None)
-        try:
-            tododatafile.drop(tododatafile.index[delete_task_index], inplace=True)
-        except IndexError:
-            print("Index out of bound...!!!")
-        tododatafile.to_csv("dataToDoList.csv", index=False)
-
-    """
-    remind() function will notify for reminders at or after little time of entry and should be called
-    at particular duration continuously
-    """
-
-    @staticmethod
-    def remind():
-        with open("dataToDoList.csv", "r") as tododatafile:
-            reader = tododatafile.readlines()[1:]
-            for rows in reader:
-                ToDo.tasks[(rows.split(",")[0])] = rows.split(",")[1]
-
-        for key in sorted(ToDo.tasks.keys()):
-            if parse(key) <= datetime.now():
-                print(ToDo.tasks[key])
-            else:
-                break
-        tododatafile.close()
-
-    """
-    add_task() will write the entry of reminder in file 
-    """
-
-    def add_task(self):
-        with open("dataToDoList.csv", "a") as tododatafile:
-            tododatafile.write(str(datetime(day=self.day, month=self.month, year=self.year, hour=self.hour,
-                                            minute=self.minute)) + "," + str(self.description) + "\n")
-            tododatafile.close()
-
-
 """
 if __name__ == "__main__":
     while True:
         print("======================================================")
-        print("\n1:sell\n2:buy\n3:delete\n4:Add task in ToDo\n5:Delete task in ToDo\n6:Remind tasks\n7:exit\n")
-        Entry.total_buy_sell()
+        print("\n1:sell\n2:buy\n3:delete\n4:exit\n")
+        Entry.total_buy_method()
+        Entry.total_sell_method()
         print("total sell =" + str(Entry.total_sell))
         print("total buy =" + str(Entry.total_buy))
         c = int(input(":"))
-        t = ToDo()
         e_type_of_entry = None
         if c == 1:
             e_type_of_entry = "Sell"
@@ -167,18 +78,6 @@ if __name__ == "__main__":
         elif c == 3:
             index = int(input("Enter id of entry to delete:"))
             Entry.delete(index)
-            continue
-        elif c == 4:
-            t.set_minute(
-                int(input("Enter minute:")))  # you can set hour,month,year by calling their respective setter methods
-            t.set_description(input("Enter Description:"))
-            t.add_task()
-            continue
-        elif c == 5:
-            t.delete_task(int(input("Enter id:")))
-            continue
-        elif c == 6:
-            t.remind()
             continue
         else:
             print("======================================================\n")
